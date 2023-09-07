a98857730bf379b5ecd1c6d044eb7a0a623496d6
Holmes <73396391+Holmes2002@users.noreply.github.com>
2023-09-07 13:32:33 +0700

    Update video_classification.py

--- Multi-Action-Recognition/video_classification.py
+++ Multi-Action-Recognition/video_classification.py
@@ -24,6 +24,7 @@ import evaluate
 import numpy as np
 
 
+metric = evaluate.load("accuracy")
 def compute_metrics(eval_pred):
     predictions = np.argmax(eval_pred.predictions, axis=1)
     return metric.compute(predictions=predictions, references=eval_pred.label_ids)
